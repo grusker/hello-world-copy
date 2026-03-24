cd9196adf07321f583cbdee37cb9d490cba6f869
Uğur <37528419+grusker@users.noreply.github.com>
2026-03-24 22:21:41 +0100

    Create GreetingRepository.java

---
+++ src/main/java/com/example/hello/GreetingRepository.java
@@ -0,0 +1,12 @@
+package com.example.hello;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GreetingRepository {
+
+    public String findGreeting() {
+        // In a real app, this would query a database
+        return "Hello";
+    }
+}
