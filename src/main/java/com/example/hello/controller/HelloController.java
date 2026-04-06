1bbb0de966b1a083053b87ee09577fa27b234081
Uğur <37528419+grusker@users.noreply.github.com>
2026-04-06 16:32:24 +0200

    Update HelloController.java

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
 
     private final GreetingService greetingService;
 
+    // This line is to test the long lines, so writing so gibberish comment here. Arise now, arise, Riders of Théoden! Dire deeds awake: dark is it eastward. Let horse be bridled, horn be sounded! Forth Eorlingas! Arise, arise, Riders of Théoden! Fell deeds awake: fire and slaughter! Spear shall be shaken, shield be splintered, a sword-day, a red day, ere the sun rises! Ride now, ride now! Ride to Gondor!
     public HelloController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
